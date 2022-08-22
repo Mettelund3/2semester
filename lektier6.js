b2bdfc654714473dda34ba37253c560b4654eeec
Mettelund3 <105641445+Mettelund3@users.noreply.github.com>
2022-08-22 22:27:50 +0200

    Add files via upload

---
+++ lektier6.js
@@ -0,0 +1,7 @@
+function alertme() {
+    alert("Alert!");
+};
+
+parseInt()
+//Int (integer) = number - man kan bare bruge Number() i stedet for, hvilket også er den man burde bruge
+//Int er kun hele tal og ikke decimaler - Float/ParseFloat er decimaler
