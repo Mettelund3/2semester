b2a7275561e0d271db782d5bf2b507ed7aac2f7c
Mettelund3 <Melu32113@edu.ucl.dk>
2022-08-25 08:37:03 +0200

    Create ovelsefratimeonsdag1.js

---
+++ ovelsefratimeonsdag1.js
@@ -0,0 +1,16 @@
+var x = 5;
+var y = 10;
+
+var result = x + y;
+alert(result);
+
+var x1 = "Mette";
+var h1 = " "
+var y1 = "Lund";
+
+var result1 = x1 + h1 + y1;
+var output1 = document.getElementById("output1");
+var farve = "green";
+
+output1.innerHTML=result1;
+output1.style.color = farve;
